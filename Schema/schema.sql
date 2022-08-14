23e599d067158fd85f8adf4d0015a9faad5934d3
Liam Selfors <lselfors@gmail.com>
2022-08-13 22:42:09 -0500

    Edited column types in Titles table

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -1,8 +1,8 @@
 -- Create tables for PH-EmployeeDB
 
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
+     dept_no VARCHAR NOT NULL,
+     dept_name VARCHAR NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
